93fe64930034a693c6bb68fc03cef27359aab97c
sct3 <104433049+sct3@users.noreply.github.com>
2022-10-30 11:03:07 +0900

    header
    making header navigation menu

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Aloha Kitchen</title>
+</head>
+<body>
+  <nav class="header_menu">
+    <ul class="main_nav">
+      <li>about</li>
+      <li>menu</li>
+      <li>news</li>
+      <li>location</li>
+    </ul>
+  </nav>
+  </header>
+  <div class="content-wrapper">
+    <h2>Welcome to Aloha Kitchen</h2>
+    <p>We'll make your day in this beautiful Island</p>
+  </div>
+
+</body>
+</html>
